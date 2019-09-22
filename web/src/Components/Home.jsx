bf89d0477909d0d7a2f4efe5ca1b9a41a0cbb48f
mike-petrov <mis5116@yandex.ru>
2019-09-22 03:24:14 +0300

    add CsvParse

--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CsvParse from '@vtex/react-csv-parse';
 
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			sample: 'Hi',
-		};
+		this.handleData = this.handleData.bind(this);
+	}
+
+	handleData(data) {
+		const { onRedirect } = this.props;
+		const X = [];
+		const Y = [];
+		const Z = [];
+		for (let m = 0; m < data.length; m += 1) {
+			X.push(data[m].X);
+			Y.push(data[m].Y);
+			Z.push(data[m].Z);
+		}
+		localStorage.setItem('CSV_x', JSON.stringify(X));
+		localStorage.setItem('CSV_y', JSON.stringify(Y));
+		localStorage.setItem('CSV_z', JSON.stringify(Z));
+		onRedirect('/chart');
 	}
 
 	render() {
-		const { sample } = this.state;
 		return (
 			<div>
-				{sample}
+				<CsvParse
+					keys={['X', 'Y', 'Z']}
+					onDataUploaded={this.handleData}
+					onError={this.handleError}
+					render={(onChange) => <input type="file" onChange={onChange} />}
+				/>
 			</div>
 		);
 	}
